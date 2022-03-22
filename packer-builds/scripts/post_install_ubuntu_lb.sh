ca5979b18bd0467659b51a22e6060ab6dd508d4b
Greg Eure <geure@hawk.iit.edu>
2022-03-21 22:18:26 -0500

    Enabled nginx load balancing and reverse proxy to satisfy "Completion of Load Balancer server" Trello card

--- packer-builds/scripts/post_install_ubuntu_lb.sh
+++ packer-builds/scripts/post_install_ubuntu_lb.sh
@@ -1,4 +1,4 @@
-#!/bin/bash 
+#!/bin/bash
 set -e
 set -v
 
@@ -13,7 +13,7 @@ sudo apt-get install -y nginx firewalld
 #################################################################################
 # Change the value of XX to be your team GitHub Repo
 # Otherwise your clone operation will fail
-# The command: su - vagrant -c switches from root to the user vagrant to execute 
+# The command: su - vagrant -c switches from root to the user vagrant to execute
 # the git clone command
 ##################################################################################
 su - vagrant -c "git clone git@github.com:illinoistech-itm/2022-team01m.git"
@@ -28,3 +28,11 @@ su - vagrant -c "git clone git@github.com:illinoistech-itm/2022-team01m.git"
 
 sudo firewall-cmd --zone=public --add-service=http --permanent
 sudo firewall-cmd --reload
+
+###########################################
+# Enable load-balancing and reverse proxy #
+###########################################
+sudo sed -i '/^http {/a \\n\tupstream django {\n\t\tleast_conn;\n\t\tip_hash;\n\t\tserver 192.168.56.102:8000;\n\t\tserver 192.168.56.103:8000;\n\t\tserver 192.168.56.104:8000;\n\t}\n' /etc/nginx/nginx.conf
+sudo sed -i '/^server {/a \\tserver_name stackprj.com;' /etc/nginx/sites-enabled/default
+sudo sed -i '/# First attempt/i \\t\tproxy_set_header Host $host;\n\t\tproxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;\n\t\tproxy_set_header X-Forwarded-Proto $scheme;\n\t\tproxy_pass http://django;' /etc/nginx/sites-enabled/default
+sudo sed -i '\%try_files $uri $uri/ =404;% s/^/#/' /etc/nginx/sites-enabled/default

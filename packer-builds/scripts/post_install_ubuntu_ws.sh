f0ac3620e4ec3028cd53df04323f8ba554fdc7ae
Greg Eure <geure@hawk.iit.edu>
2022-03-25 21:39:04 -0500

    Django pm2 autostart troubleshooting #3

--- packer-builds/scripts/post_install_ubuntu_ws.sh
+++ packer-builds/scripts/post_install_ubuntu_ws.sh
@@ -113,11 +113,11 @@ chmod u+x /home/vagrant/stopserver.sh
 
 # Command to create a service handler and start that javascript app at boot time
 cd /home/vagrant/website
-sudo pm2 startup
+pm2 startup
 # The pm2 startup command generates this command
 sudo env PATH=$PATH:/usr/bin /usr/lib/node_modules/pm2/bin/pm2 startup systemd -u vagrant --hp /home/vagrant
-sudo pm2 start stackprj.json
-sudo pm2 save
+pm2 start stackprj.json
+pm2 save
 # Change ownership of the .pm2 meta-files after we create them
 sudo chown vagrant:vagrant /home/vagrant/.pm2/rpc.sock /home/vagrant/.pm2/pub.sock
 

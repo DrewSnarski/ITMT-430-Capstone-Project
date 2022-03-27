65abc89558f4b8e43047c4228775645d6f844dfc
Greg Eure <geure@hawk.iit.edu>
2022-03-27 05:51:54 -0500

    Django pm2 autostart troubleshooting #22

--- packer-builds/scripts/post_install_ubuntu_ws.sh
+++ packer-builds/scripts/post_install_ubuntu_ws.sh
@@ -101,14 +101,13 @@ echo "database = $DATABASE" >> /home/vagrant/.my.cnf
 # Command to create a service handler and start that javascript app at boot time
 #cd /home/vagrant/website
 chmod u+x /home/vagrant/website/pm2-django.sh
-#su - vagrant -c "pm2 -s startup" &
+pm2 startup
 # The pm2 startup command generates this command
-#su - vagrant -c "sudo env PATH=$PATH:/usr/bin /usr/lib/node_modules/pm2/bin/pm2 startup systemd -u vagrant --hp /home/vagrant"
-#su - vagrant -c "pm2 start stackprj.json"
-#su - vagrant -c "pm2 save"
-#su - vagrant -c "pm2 stop stackprj.json"
-su - vagrant -c "source /home/vagrant/website/pm2-django.sh" &
-sleep 10
+su - vagrant -c "sudo env PATH=$PATH:/usr/bin /usr/lib/node_modules/pm2/bin/pm2 startup systemd -u vagrant --hp /home/vagrant"
+su - vagrant -c "pm2 start /home/vagrant/website/stackprj.json"
+su - vagrant -c "pm2 save"
+#su - vagrant -c "source /home/vagrant/website/pm2-django.sh" &
+#sleep 10
 
 #Start Django server script
 echo "cd ~/website" > /home/vagrant/startserver.sh

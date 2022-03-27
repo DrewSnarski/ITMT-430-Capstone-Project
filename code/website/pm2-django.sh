989c3f4e2967f5e9e036fa30a89233ea76da43aa
Greg Eure <geure@hawk.iit.edu>
2022-03-27 03:12:30 -0500

    Django pm2 autostart troubleshooting #16

---
+++ code/website/pm2-django.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+set -v
+
+# Command to create a service handler and start that javascript app at boot time
+cd /home/vagrant/website
+pm2 startup &
+sleep 5
+# The pm2 startup command generates this command
+sudo env PATH=$PATH:/usr/bin /usr/lib/node_modules/pm2/bin/pm2 startup systemd -u vagrant --hp /home/vagrant
+pm2 start stackprj.json
+pm2 save
+pm2 stop stackprj.json

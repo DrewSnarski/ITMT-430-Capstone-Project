af8bb0520184b78318f1656185947bdd643ab98e
Greg Eure <geure@hawk.iit.edu>
2022-04-15 18:53:45 -0500

    Secured database. Added missing sprint 04 code for hosts file, firewall rules.

--- packer-builds/scripts/post_install_ubuntu_db.sh
+++ packer-builds/scripts/post_install_ubuntu_db.sh
@@ -11,19 +11,21 @@ set -v
 sudo apt-get install -y mariadb-server firewalld
 
 #################################################################################
-# Update /etc/hosts file
+# Use an IF statement to determine if we are building for Proxmox Cloud server
+# 192.168.172.x or for VirtualBox 192.168.56.x
 #################################################################################
+IP=$(hostname -I | awk '{print $2}' | cut -d . -f3)
 
-echo "192.168.56.101     lb    lb.class.edu"    | sudo tee -a /etc/hosts
-echo "192.168.56.102     ws1   ws1.class.edu"   | sudo tee -a /etc/hosts
-echo "192.168.56.103     ws2   ws2.class.edu"   | sudo tee -a /etc/hosts
-echo "192.168.56.104     ws3   ws3.class.edu"   | sudo tee -a /etc/hosts
-echo "192.168.56.105     db    db.class.edu"    | sudo tee -a /etc/hosts
-
-#################################################################################
-# Set hostname
-#################################################################################
-sudo hostnamectl set-hostname db
+if [ $IP != 172 ]
+then
+  echo "Building for Proxmox Cloud Environment -- we have Dynamic DNS, no need for /etc/hosts files"
+else
+  echo "192.168.56.101     team-$NUMBER-lb-vm0    team-$NUMBER-lb-vm0.service.consul"    | sudo tee -a /etc/hosts
+  echo "192.168.56.102     team-$NUMBER-ws-vm0   team-$NUMBER-ws-vm0.service.consul"   | sudo tee -a /etc/hosts
+  echo "192.168.56.103     team-$NUMBER-ws-vm1   team-$NUMBER-ws-vm1.service.consul"   | sudo tee -a /etc/hosts
+  echo "192.168.56.104     team-$NUMBER-ws-vm2   team-$NUMBER-ws-vm2.service.consul"   | sudo tee -a /etc/hosts
+  echo "192.168.56.105     team-$NUMBER-db-vm0    team-$NUMBER-db-vm0.service.consul"    | sudo tee -a /etc/hosts
+fi
 
 #################################################################################
 # Change the value of XX to be your team GitHub Repo
@@ -43,9 +45,17 @@ su - vagrant -c "git clone git@github.com:illinoistech-itm/2022-team01m.git"
 # https://en.wikipedia.org/wiki/Sed
 #################################################################################
 
-# If using mysql instead of MariaDB the path to the cnf file is /etc/mysql/mysql.conf.d/mysql.cnf
-# The command: $(cat /etc/hosts | grep db | awk '{ print $1 }') will retrieve the IP address of the db from the /etc/hosts file, a bit of a hack...
-sudo sed -i "s/.*bind-address.*/bind-address = $(cat /etc/hosts | grep db | awk '{ print $1 }')/" /etc/mysql/mariadb.conf.d/50-server.cnf
+if [ $IP != 172 ]
+then
+  # Detect if we are in the Vagrant environement (third IP octet will be 56) or Proxmox environment -- will be 172
+  # If using mysql instead of MariaDB the path to the cnf file is /etc/mysql/mysql.conf.d/mysql.cnf
+  # The command: $(cat /etc/hosts | grep db | awk '{ print $1 }') will retrieve the IP address of the db from the /etc/hosts file, a bit of a hack...
+  # sudo sed -i "s/.*bind-address.*/#bind-address = $(cat /etc/hosts | grep team-$NUMBER-db-vm0 | awk '{ print $1 }')/" /etc/mysql/mysql.conf.d/mysql.cnf
+  sudo sed -i "s/.*bind-address.*/bind-address = $(cat /etc/hosts | grep team-$NUMBER-db-vm0 | awk '{ print $1 }')/" /etc/mysql/mariadb.conf.d/50-server.cnf
+else
+  # Kind of a hack....
+  sudo sed -i "s/.*bind-address.*/bind-address = 0.0.0.0/" /etc/mysql/mariadb.conf.d/50-server.cnf
+fi
 
 #################################################################################
 # Using sed to replace placeholder variables in the code/db-samples/*.sql files
@@ -65,14 +75,37 @@ sudo mysql < $TEAMREPO/create-database.sql
 sudo mysql < $TEAMREPO/create-user.sql
 sudo mysql $DATABASE < $TEAMREPO/database.sql
 
+##################################################################################
+# Secure the MariaDB (Greg E)                                             #
+##################################################################################
+mysql -sfu root <<EOS
+-- set root password
+UPDATE mysql.user SET Password=PASSWORD('$USERPASS') WHERE User='root';
+-- delete anonymous users
+DELETE FROM mysql.user WHERE User='';
+-- delete remote root capabilities
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+-- drop database 'test'
+DROP DATABASE IF EXISTS test;
+-- also make sure there are lingering permissions to it
+DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
+-- make changes immediately
+FLUSH PRIVILEGES;
+EOS
+
 #################################################################################
 # Linux systemd Firewall - firewalld https://firewalld.org/
 # Remember to open proper firewall ports
 #################################################################################
 # Open firewall port for port 3306/tcp
 sudo firewall-cmd --zone=public --add-port=3306/tcp --permanent
-# Open firewall port to allow only connections from 192.168.56.0/24
-sudo firewall-cmd --zone=public --add-source=192.168.56.0/24 --permanent
-sudo firewall-cmd --zone=public --add-source=192.168.172.0/24 --permanent
+if [ $IP != 172 ]
+then
+  # (Vagrant) Open firewall port to allow only connections from 192.168.56.0/24
+  sudo firewall-cmd --zone=public --add-source=192.168.56.0/24 --permanent
+else
+  # (Proxmox) Open firewall port to allow only connections from 192.168.172.0/24
+  sudo firewall-cmd --zone=public --add-source=192.168.172.0/24 --permanent
+fi
 # Reload changes to firewall
 sudo firewall-cmd --reload
